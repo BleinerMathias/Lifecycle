66e72cdc15444a79d602bda3d66c60902c676635
Bleiner Mathias <bleinerbbm@hotmail.com>
2023-11-06 22:31:22 -0300

    Implementando onSaveInstanceState e onRestoreInstanceState
    Implementando métodos de onSaveInstanceState(), que permite com que possamos armazenar dados em bundle quando o sistema operacional destroy telas e o onRestoreInstanceState() para restauras os dados.
    
    Lembrando que o só será executado o onSaveInstanceState(), quando o SO destruir executar onDestroy(), caso o usuário clicar em back, esse método não é executado.

--- app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/MainActivity.kt
@@ -3,7 +3,10 @@ package br.edu.scl.ifsp.sdm.lifecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.widget.EditText
+import androidx.core.view.children
 import androidx.core.widget.doAfterTextChanged
 import br.edu.scl.ifsp.sdm.lifecycle.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.lifecycle.databinding.TilePhoneBinding
@@ -14,6 +17,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var filledChars:Int = 0 // Variavel que conta as vezes que modificamos o name edittext
+
+    //Criando (singleton)
+    companion object{
+        const val FILLED_CHARS = "FILLER_CHARS"
+        const val PHONES = "PHONES"
+    }
+
+
 
     // onCreate inicializa o ciclo de vida
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +59,53 @@ class MainActivity : AppCompatActivity() {
 
         Log.v(getString(R.string.app_name), "Main - onCreate(): INÍCIO COMPLETO")
 
+    }
+
+    // Salvar tudo que desejo restaurar no futuro (atentar-se - não funciona se usuário clicar em back)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.v(getString(R.string.app_name), "Main - onSaveInstanceState(): Salvando dados")
+
+        outState.putInt(FILLED_CHARS, filledChars)
+
+        // Para telefones, sendo view dinâmicas (como não há lista), temos que percorrer nossas views
+        val phones = mutableListOf<String>()
+
+        // let trata o valor como it
+        activityMainBinding.phonesLl.children.forEachIndexed{index,view ->
+            if(index != 0){
+                (view as EditText).text.toString().also{
+                    phones.add(it)
+                }
+            }
+        }
+
+        // typeArray => retorna um array de string
+        outState.putStringArray(PHONES, phones.toTypedArray())
+    }
+
+    // Restaurar os dados que estão em outState
+    override fun onRestoreInstanceState(savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+
+        savedInstanceState.getInt(FILLED_CHARS, 0).let {
+            filledChars = it
+
+            "${getString(R.string.filled_chars)} ${filledChars}".also{fc->
+                activityMainBinding.filledCharsTv.text = fc
+            }
+
+        }
+
+        savedInstanceState.getStringArray(PHONES)?.forEach { phone->
+
+            // Adiciona um objeto inteiro de telefone na view
+            TilePhoneBinding.inflate(layoutInflater).apply {
+                root.setText(phone)
+                activityMainBinding.phonesLl.addView(root)
+            }
+        }
     }
 
     override fun onStart() {
